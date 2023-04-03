ee5edd69e3cd87864965415a7ce5fe94249db2bb
Shiney <stopthewalkingdead@gmail.com>
2023-04-03 12:55:34 +0300

    move primitives' serde impls to a separate file

---
+++ src/serde/primitives.rs
@@ -0,0 +1,100 @@
+use super::Deserialize;
+use super::Serialize;
+
+#[macro_export]
+macro_rules! impl_ne_serde {
+    ($T:ty) => {
+        impl $crate::serde::Serialize for $T {
+            #[inline(always)]
+            fn extend_serialized(&self, out: &mut Vec<u8>) {
+                out.extend(self.to_ne_bytes());
+            }
+
+            #[inline(always)]
+            fn serialized_len(&self) -> usize {
+                std::mem::size_of::<$T>()
+            }
+
+            #[inline(always)]
+            fn serialize(&self) -> Vec<u8> {
+                self.to_ne_bytes().into()
+            }
+        }
+
+        impl $crate::serde::Deserialize for $T {
+            #[inline(always)]
+            fn deserialize(bytes: &[u8]) -> $T {
+                let bytes = bytes[0..std::mem::size_of::<$T>()].try_into().expect(concat!(
+                    "expected size_of<",
+                    stringify!($T),
+                    ">() bytes for `from_ne_bytes`"
+                ));
+                <$T>::from_ne_bytes(bytes)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! impl_0_serde {
+    ($T:ty) => {
+        impl $crate::serde::Serialize for $T {
+            #[inline(always)]
+            fn extend_serialized(&self, out: &mut Vec<u8>) {
+                self.0.extend_serialized(out)
+            }
+
+            #[inline(always)]
+            fn serialized_len(&self) -> usize {
+                self.0.serialized_len()
+            }
+
+            #[inline(always)]
+            fn serialize(&self) -> Vec<u8> {
+                self.0.serialize()
+            }
+        }
+
+        impl $crate::serde::Deserialize for $T {
+            #[inline(always)]
+            fn deserialize(bytes: &[u8]) -> $T {
+                Self(<_>::deserialize(bytes))
+            }
+        }
+    };
+}
+
+impl_ne_serde!(i8);
+impl_ne_serde!(i16);
+impl_ne_serde!(i32);
+impl_ne_serde!(i64);
+impl_ne_serde!(i128);
+impl_ne_serde!(isize);
+impl_ne_serde!(u8);
+impl_ne_serde!(u16);
+impl_ne_serde!(u32);
+impl_ne_serde!(u64);
+impl_ne_serde!(u128);
+impl_ne_serde!(usize);
+impl_ne_serde!(f32);
+impl_ne_serde!(f64);
+
+impl Serialize for bool {
+    fn extend_serialized(&self, out: &mut Vec<u8>) {
+        out.push(*self as u8);
+    }
+
+    fn serialized_len(&self) -> usize {
+        1
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        vec![*self as u8]
+    }
+}
+
+impl Deserialize for bool {
+    fn deserialize(bytes: &[u8]) -> Self {
+        bytes[0] != 0
+    }
+}
